3d832598fd245d858ebc7fd6ed968d1bd8853146
Matsiiovska <125771191+Matsiiovska@users.noreply.github.com>
2023-09-15 15:06:17 +0300

    html for

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,27 +35,27 @@ class ContactForm extends React.Component {
   render() {
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Label htmlFor={this.nameInputId}>
+        <Label >
           Name
           <Input
             type="text"
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
         </Label>
 
-        <Label htmlFor={this.numberInputId}>
+        <Label>
           Number
           <Input
             type="tel"
             name="number"
             value={this.state.number}
             onChange={this.handleChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
